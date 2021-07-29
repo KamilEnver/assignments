45742e8778d5e335b73aa48ef0efcded8a26cd30
barney <barneyneilstinson@gmail.com>
2021-07-29 18:31:47 +0300

    2. Çözüm eklendi.

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -7,4 +7,19 @@ while summ < 55:
   fibonacci_list.append(summ)
   number1 = number2
   number2 = summ
-print(fibonacci_list)
+print(fibonacci_list) 
+
+2. Çözüm
+
+liste = []
+n = 0
+for i in range(10):
+  if (i==0) or (i==1):
+    n = 1
+    liste.append(n)
+  else:
+    n = liste[i-2]+liste[i-1]
+    liste.append(n)
+print("Fibonacci's first ten numbers:",liste)
+
+
