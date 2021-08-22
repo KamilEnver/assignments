9b408fcc3b54ea38ace6a936e411d77802b12c1c
barney <barneyneilstinson@gmail.com>
2021-08-22 19:32:16 +0300

    password.py created

---
+++ password.py
@@ -0,0 +1,8 @@
+a = "Kamil"
+user = input("give me your name: ").strip().capitalize()
+password_kamil = "aliyanun@123"
+if user == a:
+  print("Hello, Kamil! The password is :",password_kamil)
+else:
+  print("Hello",user,"!See you later.")
+
